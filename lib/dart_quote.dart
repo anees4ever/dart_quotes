c7ad5bc3a4bf0360b01fd992a22e6260a0cb866a
Heshan Sandeepa <heshan1598@gmail.com>
2022-09-04 21:26:45 +0530

    proceed

--- lib/dart_quote.dart
+++ lib/dart_quote.dart
@@ -1,7 +1,11 @@
 library dart_quote;
 
-/// A Calculator.
+/// A Calculator. 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
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
+
+
+
+git remote set-url origin https://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@github.com/HeshanSandeepa/dart_quotes.git
