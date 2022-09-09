ecb407ae3e52cb149dc4a531e56ea470b6f8588d
Heshan Sandeepa <heshan1598@gmail.com>
2022-09-09 10:12:40 +0530

    proceed

--- lib/dart_quote.dart
+++ lib/dart_quote.dart
@@ -3,32 +3,51 @@ library dart_quote;
 import 'package:flutter/material.dart';
 
 class DartQuote extends StatelessWidget {
-  final Text text; // respective text that need to be to wrapped with quotes
+  final String text; // respective text that need to be to wrapped with quotes
+  final TextStyle textStyle; //  quote style as preferred.
   final TextStyle quoteStyle; //  quote style as preferred.
-  final EdgeInsetsGeometry padding; // padding for the text widget
+  final EdgeInsets padding; // padding for the text widget
 
-  const DartQuote(
+   DartQuote(
       {required this.text,
+      this.textStyle = const TextStyle(
+          color: Colors.black, fontWeight: FontWeight.normal, fontSize: 40),
       this.quoteStyle = const TextStyle(
           color: Colors.black, fontWeight: FontWeight.bold, fontSize: 40),
-      this.padding = const EdgeInsets.fromLTRB(32.0, 16.0, 32.0, 16.0)});
+      this.padding = const EdgeInsets.fromLTRB(32.0, 0.0, 32.0, 40),});
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Row(
-          children: [Text('" ', style: quoteStyle), const Spacer()],
+        Container(
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Row(
+              children: [Text('\u201F', style: quoteStyle), const Spacer()],
+            ),
+          ),
         ),
-        Padding(
-          padding: padding,
-          child: text,
+        Container(
+          color: Colors.green,
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(32.0, 0.0, 32.0, quoteStyle.fontSize!),
+            child: Text(text, textAlign: TextAlign.center, style: textStyle),
+          ),
         ),
-        Row(
-          children: [
-            const Spacer(),
-            Text('" ', style: quoteStyle),
-          ],
+        Container(
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: Row(
+              children: [
+                const Spacer(),
+                Text('\u201D', style: quoteStyle),
+              ],
+            ),
+          ),
         ),
       ],
     );
