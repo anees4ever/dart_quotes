18ecfe2c1281001d727b5de482714295e6b0a778
Heshan Sandeepa <heshan1598@gmail.com>
2022-09-21 00:41:22 +0530

    proceed

---
+++ lib/string_quote.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'abstract_quote.dart';
+
+class StringQuote extends DartQuote {
+  final String text;
+  final TextStyle textStyle;
+
+  StringQuote({
+    required this.text,
+    this.textStyle = const TextStyle(
+        color: Colors.black, fontWeight: FontWeight.normal, fontSize: 20),
+  });
+
+  @override
+  quote() {
+    return Text('\u275D ' + text + ' \u275E');
+  }
+}
